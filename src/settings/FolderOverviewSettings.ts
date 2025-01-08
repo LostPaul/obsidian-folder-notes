abc72d32160a36c0289f6946adcfd7387a2a87ae
Lost Paul <70213368+LostPaul@users.noreply.github.com>
2025-01-08 16:14:36 +0100

    Edit text

--- src/settings/FolderOverviewSettings.ts
+++ src/settings/FolderOverviewSettings.ts
@@ -8,7 +8,7 @@ export async function renderFolderOverview(settingsTab: SettingsTab) {
     const { plugin } = settingsTab;
     let overviewSettings = plugin.settings.defaultOverview;
     const containerEl = settingsTab.settingsPage;
-    containerEl.createEl('p', { text: 'Change the default settings for folder overviews', cls: 'setting-item-description' });
+    containerEl.createEl('p', { text: 'Edit the default settings for folder overviews', cls: 'setting-item-description' });
 
     createOverviewSettings(containerEl, overviewSettings, plugin, plugin.settings.defaultOverview, settingsTab.display, undefined, undefined, undefined, settingsTab );
 }
